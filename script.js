3916d32597677de3998d9855a6f4f717876c1591
mohan_chandra <129154398+Ravisupreme-15@users.noreply.github.com>
2023-12-23 16:15:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
 	for (let index = 0; index < s1.length - s2.length+1; index++) {
-		if (s1.subString(index, s2.length+index) == s2) {
-			return index;
+		if (s1.substring(index, s2.length+index) == s2) {
+			return index; 
 		}
 	}
-	return -1;
+	return -1; 
 }
 
 // Please do not change the code below
-// const s1 = prompt("Enter s1:");
-// const s2 = prompt("Enter s2:");
-const s1 = "rathan";
-const s2 = "han";
+const s1 = prompt("Enter s1:"); 
+const s2 = prompt("Enter s2:");
 alert(indexOfIgnoreCase(s1, s2));
